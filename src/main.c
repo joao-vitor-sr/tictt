edf7af7444e6649e495f3e3b98529d2ad3ab18c4
Joao Vitor <joaovitorsrs7@gmail.com>
2021-10-13 08:45:51 -0300

    Now the game get the configuration of player.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 // headers
 #include "tictt.h"
+#include "main.h"
 
 int main() 
 {
-	display_board(0);
+	get_player_configuration();
 	return 1;
+}
+
+void get_player_configuration() 
+{
+	printf("What is your name? ");
+	char line_first_player[253];
+
+	if (fgets(line_first_player, sizeof(line_first_player), stdin) == NULL) {
+		name_of_first_player = "player-one";
+	}
+
+	name_of_first_player = line_first_player;
+
+	if (!player_is_playing_alone()) { 
+		player_is_alone = false;
+
+		printf("What is the second player name? ");
+		char line_second_player[253];
+
+		if (fgets(line_second_player, sizeof(line_second_player), stdin) == NULL) {
+			name_of_second_player = "player-two";
+		}
+
+		name_of_second_player = line_second_player;
+	}
+
+}
+
+bool player_is_playing_alone() 
+{
+	printf("Will you play alone? (Y/n): ");
+
+	char line[3];
+
+	if (fgets(line, sizeof(line), stdin) != NULL && 
+	(line[0] == 'Y' || line[0] == 'y')) {
+		return true;
+	}
+
+	return false;
 }
---
+++ src/main.h
@@ -0,0 +1,8 @@
+bool player_is_alone = true;
+short difficulty = 1; // 1 -- 3
+char *name_of_first_player;
+char *name_of_second_player;
+
+int main();
+bool player_is_playing_alone();
+void get_player_configuration();
