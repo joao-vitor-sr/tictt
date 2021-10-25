f7ec703d5e895607479b6849af5cc51ab1b39edb
Joao Vitor <joaovitorsrs7@gmail.com>
2021-10-25 08:58:57 -0300

    Now the bot attack is split into a different file

---
+++ src/bot_attack.h
@@ -0,0 +1,5 @@
+bool bot_line_attack();
+bool bot_column_attack();
+bool bot_diagonal_attack();
+void bot_random_attack();
+bool unique_element(char array[], short array_index, char char_to_compare);
