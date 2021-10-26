17cbdfe06ea1276f9decff2d9e86785abd1e5fd9
Joao Vitor <joaovitorsrs7@gmail.com>
2021-10-26 20:26:47 -0300

    Now the bot.c supports the new functions names

--- src/bot.c
+++ src/bot.c
@@ -1,147 +1,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-
 #include "bot.h"
 #include "tictt.h"
-
-bool bot_line_movement()
-{
-	// line
-	for (short index_line = 0; index_line < 3; index_line++) {
-		bool line_is_invalid = false;
-		// validating if at line exist some charater of the player
-		for (short index_column = 0; index_column < 3; index_column++) {
-			if (board[index_line][index_column] == 'X') {
-				line_is_invalid = true;
-				break;
-			}
-
-		}
-
-		if (line_is_invalid) {
-			line_is_invalid = false;
-			continue;
-		}
-
-		// get the last empty square
-		for (short index_column = 0; index_column < 3; index_column++) {
-			if (board[index_line][index_column] == '\0') {
-				board[index_line][index_column] = 'O';
-				return true;
-				break;
-			}
-		}
-		break;
-	}
-
-	return false;
-}
-
-bool bot_column_movement()
-{
-	// column
-	for (short index_column = 0; index_column < 3; index_column++) {
-		bool column_is_invalid = false;
-		// validating if at line exist some charater of the player
-		for (short index_line = 0; index_line < 3; index_line++) {
-			if (board[index_line][index_column] == 'X') {
-				column_is_invalid = true;
-				break;
-			}
-
-		}
-
-		if (column_is_invalid) {
-			column_is_invalid = false;
-			continue;
-		}
-
-		// get the last empty square
-		for (short index_line = 0; index_line < 3; index_line++) {
-			if (board[index_line][index_column] == '\0') {
-				board[index_line][index_column] = 'O';
-				return true;
-				break;
-			}
-		}
-		break;
-	}
-
-	return false;
-}
-
-bool bot_diagonal_movement()
-{
-
-	// -- diagonal --
-
-	// diagonal (right to left)
-	bool diagonal_right_to_left_is_valid = true;
-	if (board[0][2] == 'X' ||
-			board[1][1] == 'X' || 
-			board[2][0] == 'X') {
-		diagonal_right_to_left_is_valid = false;
-
-	}
-
-	if (diagonal_right_to_left_is_valid) {
-		// get the last empty square
-		bool still_is_searching = true;
-
-		unsigned short line_index = 0;
-		unsigned short column_index = 2;
-		while (still_is_searching) {
-			if (board[line_index][column_index] == '\0') {
-				board[line_index][column_index] = 'O';
-				return true;
-			}
-
-			if (column_index == 0) {
-				still_is_searching = false;
-				break;
-			}
-
-			column_index--;
-			line_index++;
-		}
-
-	}
-
-	// diagonal (left to right)
-	bool diagonal_left_to_right_is_valid = true;
-	if (board[0][0] == 'X' ||
-			board[1][1] == 'X' || 
-			board[2][2] == 'X') {
-		diagonal_left_to_right_is_valid = false;
-
-	}
-
-	if (diagonal_left_to_right_is_valid) {
-		// get the last empty square
-		bool still_is_searching = true;
-
-		unsigned short line_index = 0;
-		unsigned short column_index = 0;
-		while (still_is_searching) {
-			if (board[line_index][column_index] == '\0') {
-				board[line_index][column_index] = 'O';
-				return true;
-			}
-
-			if (column_index == 0) {
-				still_is_searching = false;
-				break;
-			}
-
-			column_index++;
-			line_index++;
-		}
-
-	}
-
-	return false;
-}
+#include "bot_defense.h"
+#include "bot_attack.h"
 
 bool unique_element(char array[], short array_index, char char_to_compare)
 {
@@ -157,176 +20,127 @@ bool unique_element(char array[], short array_index, char char_to_compare)
 bool bot_defense() 
 {
 	for (short index = 0; index < 3; index++) {
-
-		// line
-		bool line_is_safe = false;
-		for (short index_column = 0; index_column < 3; index_column++) {
-			if (board[index][index_column] == 'O') {
-				line_is_safe = true;
-			}
+		if (defense_line(index)) {
+			return true;
 		}
 
-		if (unique_element(board[index], 3, '\0')) {
-			line_is_safe = true;
+		if (defense_column(index)) {
+			return true;
 		}
 
-		// validating if the player is close to complete the line
-		short quantity_of_invalid_squares = 0;
-		for (short index_column = 0; index_column < 3; index_column++) {
-			if (board[index][index_column] == 'X') {
-				quantity_of_invalid_squares++;
-			}
-		}
-
-		if (!line_is_safe && quantity_of_invalid_squares == 2) {
-			for (short index_column = 0; index_column < 3; index_column++) {
-				if (board[index][index_column] != 'X') {
-					board[index][index_column] = 'O';
-					return true;
-					break;
-				}
-			}
-		}
-
-
-		// column
-		bool column_is_safe = false;
-		char column_array[3];
-		for (short index_line = 0; index_line < 3; index_line++) {
-			column_array[index_line] = board[index_line][index];
-			if (board[index_line][index] == 'O') {
-				column_is_safe = true;
-			}
-		}
-
-		if (unique_element(column_array, 3, '\0')) {
-			column_is_safe = true;
-		}
-
-		// validating if the player is close to complete the line
-		short quantity_of_invalid_squares_column = 0;
-		for (short index_line = 0; index_line < 3; index_line++) {
-			if (board[index_line][index] == 'X') {
-				quantity_of_invalid_squares_column++;
-			}
-		}
-
-		if (!column_is_safe && quantity_of_invalid_squares_column == 2) {
-			for (short index_line = 0; index_line < 3; index_line++) {
-				if (board[index_line][index] != 'X') {
-					board[index_line][index] = 'O';
-					return true;
-					break;
-				}
-			}
-		}
 	}
 
 	// diagonal
 
-	// left to right
-	// get the last empty square
-	bool still_is_searching_left_to_right = true;
-
-	unsigned short line_index = 0;
-	unsigned short column_index = 0;
-	unsigned short invalid_diagonal_squares_left = 0;
-	bool diagonal_left_to_right_is_safe = false;
-	while (still_is_searching_left_to_right) {
-		if (board[line_index][column_index] == 'O') {
-			diagonal_left_to_right_is_safe = true;
-			still_is_searching_left_to_right = false;
-			break;
-		} else if (board[line_index][column_index] == 'X') {
-			invalid_diagonal_squares_left++;
-		}
-
-
-		line_index++;
-		column_index++;
-
-		if (line_index == 3) {
-			still_is_searching_left_to_right = false;
-		}
+	if (defense_diagonal_left_to_right()) {
+		return true;
 	}
 
-
-	if (!diagonal_left_to_right_is_safe && invalid_diagonal_squares_left == 2) {
-		unsigned short line_index = 0;
-		unsigned short column_index = 0;
-		bool diagonal_left_to_right_is_safe = false;
-		bool still_is_searching_left_to_right = true;
-		while (still_is_searching_left_to_right) {
-			if (board[line_index][column_index] == '\0') {
-				board[line_index][column_index] = 'O';
-				still_is_searching_left_to_right = false;
-				return true;
-			}
-
-			line_index++;
-			column_index++;
-
-
-			if (line_index == 3) {
-				still_is_searching_left_to_right = false;
-			}
-		}
-	}
-
-	// right to left
-	// get the last empty square
-	bool still_is_searching_right_to_left = true;
-
-	line_index = 0;
-	column_index = 2;
-	unsigned short invalid_diagonal_squares_right = 0;
-	bool diagonal_right_to_left_is_safe = false;
-	while (still_is_searching_right_to_left) {
-		if (board[line_index][column_index] == 'O') {
-			diagonal_right_to_left_is_safe = true;
-			still_is_searching_right_to_left = false;
-			break;
-		} else if (board[line_index][column_index] == 'X') {
-			invalid_diagonal_squares_right++;
-		}
-
-
-		line_index++;
-		column_index--;
-
-		if (line_index == 3) {
-			still_is_searching_right_to_left = false;
-		}
-	}
-
-
-	if (!diagonal_left_to_right_is_safe && invalid_diagonal_squares_right == 2) {
-		unsigned short line_index = 0;
-		unsigned short column_index = 0;
-		bool diagonal_left_to_right_is_safe = false;
-		bool still_is_searching_left_to_right = true;
-		while (still_is_searching_left_to_right) {
-			if (board[line_index][column_index] == '\0') {
-				board[line_index][column_index] = 'O';
-				still_is_searching_left_to_right = false;
-				return true;
-			}
-
-			line_index++;
-			column_index++;
-
-
-			if (line_index == 3) {
-				still_is_searching_left_to_right = false;
-			}
-		}
+	if (defense_diagonal_right_to_left()) {
+		return true;
 	}
 
 	return false;
 }
 
-void make_bot_movement() 
+void make_bot_attack() 
 {
+
+	for (short index = 0; index < 3; index++) {
+
+		// line
+		short line_bot_mark = 0;
+		for (short index_column = 0; index_column < 3; index_column++) {
+			if (board[index][index_column] == 'O') {
+				line_bot_mark++;
+			}
+		}
+
+		if (line_bot_mark == 2) {
+			if (bot_line_attack()) {
+				return;
+				break;
+			}
+		}
+
+		// column
+		short column_bot_mark = 0;
+		for (short index_line = 0; index_line < 3; index_line++) {
+			if (board[index_line][index] == 'O') {
+				column_bot_mark++;
+			}
+		}
+
+		if (column_bot_mark == 2) {
+			if (bot_column_attack()) {
+				return;
+				break;
+			}
+		}
+
+	}
+
+	// diagonal
+
+	bool still_is_searching_diagonal_right_to_left = true;
+
+	short index_line = 0;
+	short index_column = 2;
+	short column_bot_mark_right_left = 0;
+
+	while (still_is_searching_diagonal_right_to_left) {
+		if (board[index_line][index_column] == 'O') {
+			column_bot_mark_right_left++;
+		}
+
+		if (bot_diagonal_attack()) {
+			return;
+		}
+
+		if (index_line <= 2) {
+			index_line++;
+			index_column--;
+			continue;
+		}
+
+		still_is_searching_diagonal_right_to_left = false;
+
+		break;
+	}
+
+	if (column_bot_mark_right_left == 2) {
+		bot_diagonal_attack();
+	}
+
+	bool still_is_searching_diagonal_left_to_right = true;
+
+	index_line = 0;
+	index_column = 0;
+	column_bot_mark_right_left = 0;
+
+	while (still_is_searching_diagonal_left_to_right) {
+		if (board[index_line][index_column] == 'O') {
+			column_bot_mark_right_left++;
+		}
+
+		if (bot_diagonal_attack()) {
+			return;
+		}
+
+		if (index_line <= 2) {
+			index_line++;
+			index_column++;
+			continue;
+		}
+
+		still_is_searching_diagonal_left_to_right = false;
+
+		break;
+	}
+
+	if (column_bot_mark_right_left == 2) {
+		bot_diagonal_attack();
+	}
 
 	// defense
 	if (bot_defense()) {
@@ -334,19 +148,20 @@ void make_bot_movement()
 	}
 
 	// line
-	if (bot_line_movement()) {
+	if (bot_line_attack()) {
 		return;
 	}
 
 	// column
-	if (bot_column_movement()) {
+	if (bot_column_attack()) {
 		return;
 	}
 
 	// diagonal
-	if (bot_diagonal_movement()) {
+	if (bot_diagonal_attack()) {
 		return;
 	}
 
 	// TIE
+	bot_random_attack();
 }
--- src/bot.h
+++ src/bot.h
@@ -1,5 +1,3 @@
-void make_bot_movement();
-bool bot_line_movement();
-bool bot_diagonal_movement();
+void make_bot_attack();
 bool bot_defense();
 bool unique_element(char array[], short array_index, char char_to_compare);
